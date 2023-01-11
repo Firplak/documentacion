48954e28efd36caf93351868cc46aaf8fc7c0945
Jhonatan David <jhonatandaar@gmail.com>
2023-01-11 11:46:08 -0500

    fix: changed baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Firplak',
   tagline: 'Documentacion de TI',
   url: 'https://docs.firplak.com',
-  baseUrl: '/',
+  baseUrl: '/documentacion',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
